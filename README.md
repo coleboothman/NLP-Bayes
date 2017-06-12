1d80f3a4c9853e7688271116549e94116afa055f
Cole Boothman <coleboothman2@gmail.com>
2017-06-12 01:32:11 -0700

    added larger datasets and updated README

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # NLP-Bayes
 Natural Language Processing Naive Bayes Classifier for predicting sentiment in movie reviews
+
+
+Remarks (June 12, 2017):
+
+There are two sets of training data, small: neg.csv, pos.csv and large: neg_large.csv, pos_large.csv
+The small datasets contain 5331 reviews in neg.csv, and 5331 reviews in pos.csv
+Originally, 5000 from each .csv file were used to train the model, and the remaining 331 reviews used to test accuracy of the Classifier.
+
+When using the small file, I noticed that perhaps there wasn't enough data for the data to computer predictions accurately.
+For example, when given a review "This was a good movie" the machine predicted this was a negative review.
+
+After using the large file I noticed that user input was predicted correct more often.
+However, when I used the small files as test data for the classifer, it predicited positive correct very often, 
+but predicited negative badly.
+
+This may also be because the large datasets are not the same size. (approx. 41,000 Positive Reviews, 33,000 Negative Reviews).
+Because we use the # of reviews in each class for calculations this may skewer the results a bit.
+
+I'll be doing more testing next week when I have some time.
+
+Currently the classifier is setup to take user input reviews after the model has been trained. The function test_data() has been commented 
+out for now.
+
+Noticing that common words have a very large discrepancy in how many times they may appear in a review class: (ie, the appears over 20,000 times in negative
+reviews, but the appears only 14,000 times in positive reviews).
--- bayes.py
+++ bayes.py
@@ -7,9 +7,18 @@ import time
 import sys
 
 def get_text(reviews):
-	 return " ".join([r[0].lower() for r in reviews])	
+	 #for small files, (neg.csv and pos.csv)
+	 #return " ".join([r[0].lower() for r in reviews])
+	 
+	text = " "
+	for row in range(len(reviews)):
+		s = "".join(reviews[row])
+		text += "".join(s.lower())
+		text += " "
 
 
+	return text	
+	 
 
 def get_counts(text):
 	words = re.split("\s+", text)
@@ -25,7 +34,7 @@ def remove_punc(reviews):
 
 
 
-def train(total_pos_words, total_neg_words, counter_p, counter_n):
+def train(total_words, total_pos_words, total_neg_words, counter_p, counter_n):
 	start_time = time.time()
 	print "Starting to train classifer"
 
@@ -34,12 +43,12 @@ def train(total_pos_words, total_neg_words, counter_p, counter_n):
 
 	for feature in counter_n:
 		feature_count = counter_n[feature]
-		feat_prob_neg[feature] = float((feature_count+1)/total_neg_words)
+		feat_prob_neg[feature] = float((feature_count+1)/total_words)
 	
 
 	for feature in counter_p:
 		feature_count = counter_p[feature]
-		feat_prob_pos[feature] = float((feature_count+1)/total_pos_words)
+		feat_prob_pos[feature] = float((feature_count+1)/total_words)
 
 	print("The classifier is trained and it took %s seconds" % (time.time() - start_time))
 
@@ -47,14 +56,20 @@ def train(total_pos_words, total_neg_words, counter_p, counter_n):
 
 	
 
-def make_class_prediction(review, unique_class_feat, prob_class, feat_probs_class, num_reviews_in_class):
+def make_class_prediction(review, unique_class_feat, prob_class, feat_probs_class, num_reviews_in_class, common_words):
 	prediction = 1
 	
 	for word in range(len(review)):
 		feat = review[word]
 		if(feat in feat_probs_class.keys()):
-			prediction *= feat_probs_class[feat]
+			#NEW: Data for words like "the" and "and" are heavily favoured to one side. Remove these words.
+			
+			if(feat in common_words.keys()):
+				prediction *= (1/unique_class_feat)	
+			else:
+				prediction *= feat_probs_class[feat]
 		else:
+			
 			prediction *= (1/unique_class_feat)	
 
 
@@ -81,8 +96,8 @@ def test_data():
 	for row in range(len(reviews_neg_test)):
 		s = ''.join(reviews_neg_test[row])
 		s = s.split()
-		predict_neg = make_class_prediction(s, unique_neg, prob_class_neg, feat_prob_neg, num_neg_reviews)
-		predict_pos = make_class_prediction(s, unique_pos, prob_class_pos, feat_prob_pos, num_pos_reviews)
+		predict_neg = make_class_prediction(s, unique_neg, prob_class_neg, feat_prob_neg, num_neg_reviews, neg_common)
+		predict_pos = make_class_prediction(s, unique_pos, prob_class_pos, feat_prob_pos, num_pos_reviews, pos_common)
 
 		if (predict_neg > predict_pos):
 			final_predictions_neg.append("Negative")
@@ -105,8 +120,8 @@ def test_data():
 		s = ''.join(reviews_pos_test[row])
 		s = s.lower()
 		s = s.split()
-		predict_neg = make_class_prediction(s, unique_neg, prob_class_neg, feat_prob_neg, num_neg_reviews)
-		predict_pos = make_class_prediction(s, unique_pos, prob_class_pos, feat_prob_pos, num_pos_reviews)
+		predict_neg = make_class_prediction(s, unique_neg, prob_class_neg, feat_prob_neg, num_neg_reviews, neg_common)
+		predict_pos = make_class_prediction(s, unique_pos, prob_class_pos, feat_prob_pos, num_pos_reviews, pos_common)
 
 		if (predict_neg > predict_pos):
 			final_predictions_pos.append("Negative")
@@ -129,21 +144,41 @@ def test_data():
 	print 'Out of Positive Test reviews, the Classifier predicited ', pos_correct, ' percent correct.\n'
 
 
+# ********** FOR LARGE TESTS ************
 
-
-with open("neg.csv") as file:
+with open("neg_large.csv") as file:
 	r_neg = list(csv.reader(file))
 
-with open("pos.csv") as file:
+with open("pos_large.csv") as file:
 	r_pos = list(csv.reader(file)) 	
 
-#split the negative reviews into train data and test data
-reviews_neg = r_neg[:5000]
-reviews_neg_test = r_neg[5000:]
+reviews_neg = r_neg
+reviews_pos = r_pos
 
+#SMALL FILES ARE NOW USED FOR TESTING.
+with open("neg.csv") as file:
+	reviews_neg_test = list(csv.reader(file))
+
+with open("pos.csv") as file:
+	reviews_pos_test = list(csv.reader(file))
+
+
+# ********* FOR SMALL TESTS ****************
+#with open("neg.csv") as file:
+#	r_neg = list(csv.reader(file))
+
+#with open("pos.csv") as file:
+#	r_pos = list(csv.reader(file)) 	
+
+#reviews_neg = r_neg[:5000]
+#reviews_pos = r_pos[:5000]
+
+
+#reviews_neg_test = r_neg[5000:]
 #split the positive reviews into train data and test data
-reviews_pos = r_pos[:5000]
-reviews_pos_test = r_pos[5000:]
+#reviews_pos_test = r_pos[5000:]
+#split the negative reviews into train data and test data
+
 
 
 #remove punctuation and get counts for negative text.		
@@ -152,6 +187,7 @@ remove_punc(reviews_neg)
 neg_text = get_text(reviews_neg)
 counter_neg = get_counts(neg_text)
 
+
 #remove punctuation and get counts for positive text.
 #TRAINING DATA POS
 remove_punc(reviews_pos)
@@ -159,9 +195,15 @@ pos_text = get_text(reviews_pos)
 counter_pos = get_counts(pos_text)
 
 
+pos_common = dict(counter_pos.most_common(10))
+neg_common = dict(counter_neg.most_common(10))
+
+
+
 #Number of unique words in positive class, and unique class
 unique_pos = len(counter_pos)
 unique_neg = len(counter_neg)
+total_word_count = unique_pos+unique_neg
 
 #get number of positive and neg reviews
 num_neg_reviews = len(reviews_neg)
@@ -171,14 +213,15 @@ num_pos_reviews = len(reviews_pos)
 prob_class_pos = num_pos_reviews / (num_pos_reviews + num_neg_reviews)
 prob_class_neg = num_neg_reviews / (num_pos_reviews + num_neg_reviews)
 
-feat_prob_pos, feat_prob_neg = train(unique_pos, unique_neg, counter_pos, counter_neg)
+feat_prob_pos, feat_prob_neg = train(total_word_count, unique_pos, unique_neg, counter_pos, counter_neg)
 
 
 # ***************** TRAINING DONE  ***************************
 
+# UNCOMMENT THE BELOW LINE TO RUN TEST DATA ON NEG.CSV AND POS.CSV
 test_data()
 print "\nThis program is a Naive Bayes Classifier for predicting sentiment in movie reviews."
-print "\nIt is trained using 5000 Negative and Positive Movie reviews."
+print "\nIt is trained using over 70,000 Movie reviews."
 print "\nEnter text input and the classifier will make a prediction on whether the review is positive, or negative."
 print "\nPrint exit to quit the program."
 
@@ -194,8 +237,8 @@ while True:
 
 	review = review.lower()	
 	review = review.split()
-	predict_neg = make_class_prediction(review, unique_neg, prob_class_neg, feat_prob_neg, num_neg_reviews)
-	predict_pos = make_class_prediction(review, unique_pos, prob_class_pos, feat_prob_pos, num_pos_reviews)
+	predict_neg = make_class_prediction(review, unique_neg, prob_class_neg, feat_prob_neg, num_neg_reviews, neg_common)
+	predict_pos = make_class_prediction(review, unique_pos, prob_class_pos, feat_prob_pos, num_pos_reviews, pos_common)
 	print "\n Negative Prediction Probability: ", predict_neg
 	print "\n Positive Prediction Probability: ", predict_pos
 		
@@ -203,3 +246,5 @@ while True:
 			print "\nYour review was predicited as a negative review."
 	else:
 			print "\nYour review was predicted as a positive review."
+
+	print "\n\n\n\n\n"		
